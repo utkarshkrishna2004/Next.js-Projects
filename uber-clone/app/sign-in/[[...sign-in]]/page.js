cf78356beda96ef2fbd783c9d98f7329432c169c
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-21 18:47:02 +0530

    New Project, Uber Clone, Finished Auth from Clerk.

---
+++ uber-clone/app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,20 @@
+import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+   return (
+      <>
+         <div>
+            <Image
+               src="/uberbanner.jpg"
+               width={900}
+               height={1000}
+               className="object-contain h-full w-full opacity-60"
+            />
+            <div className="absolute top-20 right-0">
+               <SignIn />
+            </div>
+         </div>
+      </>
+   );
+}
