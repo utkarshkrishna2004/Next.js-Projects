744fa3ce6ec6e4cdf847a8d5adb557eb41667cdb
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-21 19:44:29 +0530

    Basic search, map layout done.

--- uber-clone/app/page.js
+++ uber-clone/app/page.js
@@ -3,8 +3,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <UserButton />
+    <div className='p-6 grid grid-cols-1 md:grid-cols-3 gap-5 '>
+      <div>
+        Search
+      </div>
+      <div className='col-span-2'>
+        Google Map
+      </div>
     </div>
   )
 }
