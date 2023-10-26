a132e291d8e5f69ff50b5c29d737968128d8cd3d
utkarshkrishna2004 <utkarshmark14@gmail.com>
2023-10-26 19:30:20 +0530

    Google Cloud SetUp Done, SearchBar autocomplete Done

--- uber-clone/components/Home/InputItem.js
+++ uber-clone/components/Home/InputItem.js
@@ -1,22 +1,44 @@
-import Image from "next/image";
-import React from "react";
+"use client"
 
-import PlaceIcon from "@mui/icons-material/Place";
-import PinDropIcon from "@mui/icons-material/PinDrop";
-import AdjustIcon from "@mui/icons-material/Adjust";
+import Image from "next/image";
+import React, { useState } from "react";
+
 import StopCircleIcon from "@mui/icons-material/StopCircle";
 import RadioButtonCheckedIcon from "@mui/icons-material/RadioButtonChecked";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+
 
 function InputItem({ type }) {
+   const [value, setValue] = useState(null)
    return (
       <div className="bg-slate-200 p-3 rounded-lg mt-3 flex items-center gap-4">
-         {type == "source" ? <AdjustIcon /> : <RadioButtonCheckedIcon />}
-         <input
+         {type == "source" ? <StopCircleIcon /> : <RadioButtonCheckedIcon />}
+         {/* <input
             type="text"
             placeholder={
                type == "source" ? "PickUp Location" : "DropOff Location"
             }
             className="bg-transparent w-full outline-none"
+         /> */}
+         <GooglePlacesAutocomplete
+            apiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}
+            selectProps={{
+               value,
+               onChange: setValue,
+               placeholder: "PickUp Location",
+               isClearable: true,
+               className: "w-full ",
+               components: {
+                  DropdownIndicator: false,
+               },
+               styles: {
+                  control: (provided) => ({
+                     ...provided,
+                     backgroundColor: '#00ffff00',
+                     border: 'none'
+                  }),
+               },
+            }}
          />
       </div>
    );
